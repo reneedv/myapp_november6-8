7913902aa346f3f9168767029b57bbc97c17f763
Renée De Voursney <renee@nird.us>
2013-11-08 17:52:40 -0600

    added carrierwave for user pictures

--- Gemfile
+++ Gemfile
@@ -17,6 +17,7 @@ gem 'haml-rails'
 gem 'rolify'
 gem 'simple_form'
 gem 'thin'
+gem 'carrierwave'
 group :development do
   gem 'better_errors'
   gem 'binding_of_caller', :platforms=>[:mri_19, :mri_20, :rbx]
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131108214702) do
+ActiveRecord::Schema.define(version: 20131108222155) do
 
   create_table "books", force: true do |t|
     t.string   "title"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 20131108214702) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "name"
+    t.string   "picture"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
